fc17ff41200f2ad4545a0cbb74b07fce9cc3811c
Сергей <seregabur@live.ru>
2022-06-12 22:20:49 +0300

    3

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ namespace OOP12
 
         private void GoAviary()
         {
-            string aviaryString = _menu.ChoosingAviary();
+            string aviaryString = _menu.ChoosingAnAviary();
 
             if (_zoo.IsThereAviary(aviaryString))
             {
@@ -66,7 +66,7 @@ namespace OOP12
             Console.WriteLine($"Вольер с {name}");
         }
 
-        internal string ChoosingAviary()
+        internal string ChoosingAnAviary()
         {
             Console.WriteLine("Выберите вольер для просмотра.");
             string choosing = Console.ReadLine();
